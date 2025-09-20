838b225b15b0345dfbaf8c7eaf6e70bca57ab4c7
Nashra Absar <nashraabsar151@gmail.com>
2025-09-21 01:19:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function filterVideos(category) {
+  let cards = document.querySelectorAll('.video-card');
+  cards.forEach(card => {
+    if (category === 'all' || card.classList.contains(category)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
